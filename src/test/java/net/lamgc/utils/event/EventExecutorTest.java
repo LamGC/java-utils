f5317d89aa370d49770f44e13bf6a1c829f3574a
LamGC <lam827@lamgc.net>
2019-12-23 19:53:49 +0800

    Optimize tests and add tests for removeHandler;

--- src/test/java/net/lamgc/utils/event/EventExecutorTest.java
+++ src/test/java/net/lamgc/utils/event/EventExecutorTest.java
@@ -8,9 +8,6 @@ import java.util.concurrent.TimeUnit;
 
 public class EventExecutorTest {
 
-    private static void handler(SimpleEventObject event) {
-    }
-
     @Test
     public void executorTest() throws IllegalAccessException, InterruptedException {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
@@ -27,13 +24,14 @@ public class EventExecutorTest {
             }
         });
         EventExecutor executor = new EventExecutor(threadPoolExecutor);
-        executor.addHandler(new SimpleEventHandler());
-        executor.addHandler(new SimpleEventHandler2());
-        executor.addHandler(new SimpleEventHandler3());
-        executor.addHandler(new SimpleEventHandler4());
-        executor.addHandler(new SimpleEventHandler());
+        SimpleEventHandler handler = new SimpleEventHandler();
+        executor.addHandler(handler);
         executor.executor(new SimpleEventObject(1, "HelloWorld"));
-        executor.awaitEventExecutor(10L, TimeUnit.SECONDS);
+        Thread.sleep(500L);
+        executor.removeHandler(handler);
+        System.out.println("已删除Handler");
+        executor.executor(new SimpleEventObject(1, "HelloWorld"));
+        //executor.awaitEventExecutor(10L, TimeUnit.SECONDS);
     }
 
 
--- src/test/java/net/lamgc/utils/event/SimpleEventHandler.java
+++ src/test/java/net/lamgc/utils/event/SimpleEventHandler.java
@@ -3,23 +3,7 @@ package net.lamgc.utils.event;
 public class SimpleEventHandler implements EventHandler {
 
     public void simpleEventHandler1(SimpleEventObject event){
-        System.out.println(this.getClass().getSimpleName() + " - 1[" + event.errorCode + "] " + event.msg);
-    }
-
-    public void simpleEventHandler2(SimpleEventObject event){
-        System.out.println(this.getClass().getSimpleName() + " - 2[" + event.errorCode + "] " + event.msg);
-    }
-
-    public void simpleEventHandler3(SimpleEventObject event){
-        System.out.println(this.getClass().getSimpleName() + " - 3[" + event.errorCode + "] " + event.msg);
-    }
-
-    public void simpleEventHandler4(SimpleEventObject event){
-        System.out.println(this.getClass().getSimpleName() + " - 4[" + event.errorCode + "] " + event.msg);
-    }
-
-    public void simpleEventHandler5(SimpleEventObject event){
-        System.out.println(this.getClass().getSimpleName() + " - 5[" + event.errorCode + "] " + event.msg);
+        System.out.println(this.getClass().getSimpleName() + " - [" + event.errorCode + "] " + event.msg);
     }
 
 }
