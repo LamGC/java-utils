3e740175603d88a81bf7486cfc926cabbf7e699e
LamGC <lam827@lamgc.net>
2020-06-29 18:28:19 +0800

    [Update] pom.xml 更新source插件版本, 删除无用插件; [Add] NotAccepted 补充Javadoc;

--- src/main/java/net/lamgc/utils/event/NotAccepted.java
+++ src/main/java/net/lamgc/utils/event/NotAccepted.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 标注了该注解的方法, 即使满足了EventHandler接收事件所需方法的条件, 也不会收到事件.
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NotAccepted {
